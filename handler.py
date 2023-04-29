4310cff9906201e1d62393caa6b9c039c2172046
vaamps <42648064+vaamps@users.noreply.github.com>
2023-04-29 15:35:22 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,8 +12,8 @@ output_bucket = "cse546-output"
 dynamodb_table = "cse546-ddb"
 frames_path = '/tmp/'
 
-AWS_ACCESS_KEY_ID = 'AKIAQL6AAJ62SU3E4WMV'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = <secret key>
+AWS_SECRET_ACCESS_KEY = <access key>
 
 s3_client = boto3.client('s3',
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
